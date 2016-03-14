8ff0f11ab468d691105fe5401a981e226b0539b0
Grayson Hicks <graysonhicks@gmail.com>
2016-03-14 09:30:51 -0400

    fixed c, plugin error

--- c/app/scripts/index.js
+++ c/app/scripts/index.js
@@ -10,13 +10,15 @@ var views = require('./views/views.js');
 
 var blogPosts = new models.BlogCollection();
 
-blogPosts.fetch();
 
-$(".panel-default").dotdotdot({
-  watch: true,
-  height: 50
-});
+
+// $(".panel-default").dotdotdot({
+//   watch: true,
+//   height: 50
+// });
 
 var blogView = new views.BlogPostsView({collection: blogPosts});
-
+console.log(blogView.render().el);
 $('.panel-default').append(blogView.render().el);
+
+blogPosts.fetch();
